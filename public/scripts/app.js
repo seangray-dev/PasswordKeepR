7f7e8865221547cd06a13aec8625e1184920d106
Shujie Ma <shujie1st@gmail.com>
2023-04-25 21:48:29 -0400

    Add copy to clipboard function

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,6 +2,7 @@
 const showPasswordBtn = document.querySelectorAll(".show-password-btn");
 const password = document.querySelectorAll(".hidden-password");
 const passwordPlaceholder = document.querySelectorAll(".show-password");
+const copyToClipboardBtn = document.querySelectorAll(".copy-to-clipboard");
 
 showPasswordBtn.forEach((btn, index) => {
   btn.addEventListener("click", () => {
@@ -12,5 +13,22 @@ showPasswordBtn.forEach((btn, index) => {
     // Toggle the visibility of the eye and eye-slash icons
     btn.querySelector(".eye-show").classList.toggle("hidden");
     btn.querySelector(".eye-hide").classList.toggle("hidden");
+  });
+});
+
+copyToClipboardBtn.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    navigator.clipboard.writeText(password[index].innerHTML)
+      .then(() => {
+        // Show check icon after password successfully copied to clipboard
+        btn.querySelector(".fa-clipboard").classList.toggle("hidden");
+        btn.querySelector(".fa-check").classList.toggle("hidden");
+        // Check icon change back to clipboard icon after 2 seconds
+        setTimeout(()=> {
+          btn.querySelector(".fa-clipboard").classList.toggle("hidden");
+          btn.querySelector(".fa-check").classList.toggle("hidden");
+        }, 2000)
+      })
+      .catch((err) => alert("Copy to clipboard failed!"))
   });
 });
