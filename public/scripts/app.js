f81333829414880a4b8c26695cd0490866bee423
Sean Gray <seangray.dev@gmail.com>
2023-04-26 05:28:16 -0400

    add animations to modal upon hide/show, refactor app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,51 +17,6 @@ showPasswordBtn.forEach((btn, index) => {
   });
 });
 
-// Edit password
-const editPasswordButtons = document.querySelectorAll(".edit-password");
-const modal = document.getElementById("editPasswordModal");
-const cancelButton = modal.querySelector(".cancel-edit-password");
-
-editPasswordButtons.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    // Show the modal
-    modal.style.display = "block";
-
-    // When the form is submitted, update the password and hide the modal
-    modal.querySelector("form").addEventListener("submit", (event) => {
-      event.preventDefault();
-
-      const newPassword = document.getElementById("newPassword").value;
-      const confirmNewPassword =
-        document.getElementById("confirmNewPassword").value;
-
-      if (newPassword !== confirmNewPassword) {
-        alert("New password and confirm password must match");
-        return;
-      }
-
-      // Update the password element
-      const password = document.querySelectorAll(".hidden-password")[index];
-      password.textContent = newPassword;
-
-      // Hide the modal
-      modal.style.display = "none";
-    });
-  });
-});
-
-// When the cancel button is clicked, hide the modal
-cancelButton.addEventListener("click", () => {
-  modal.style.display = "none";
-});
-
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
-    modal.style.display = "none";
-  }
-});
-
 // Copy password to clipboard
 const copyToClipboardBtn = document.querySelectorAll(".copy-to-clipboard");
 
@@ -83,13 +38,105 @@ copyToClipboardBtn.forEach((btn, index) => {
   });
 });
 
-// Delete Password
-const deletePasswordButtons = document.querySelectorAll(".delete-password");
+// Function to show modal
+function showModal(modal) {
+  modal.style.display = "block";
+}
 
-deletePasswordButtons.forEach((button, index) => {
+// Function to hide modal with fade-out animation
+function hideModalWithFadeOut(modal) {
+  modal.classList.add("fade-out");
+  setTimeout(() => {
+    modal.style.display = "none";
+    modal.classList.remove("fade-out");
+  }, 500);
+}
+
+// Edit password (also needs to update the database securely)
+const editPasswordButtons = document.querySelectorAll(".edit-password");
+const editPasswordmodal = document.getElementById("editPasswordModal");
+const cancelButton = editPasswordmodal.querySelector(".cancel-edit-password");
+
+editPasswordButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
-    // Get the password card element and remove it
-    const passwordCard = button.closest(".dashboard__passwords-card");
-    passwordCard.remove();
+    // Show the editPasswordmodal
+    showModal(editPasswordmodal);
+
+    // When the form is submitted, update the password and hide the editPasswordmodal
+    editPasswordmodal
+      .querySelector("form")
+      .addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const newPassword = document.getElementById("newPassword").value;
+        const confirmNewPassword =
+          document.getElementById("confirmNewPassword").value;
+
+        if (newPassword !== confirmNewPassword) {
+          alert("New password and confirm password must match");
+          return;
+        }
+
+        // Update the password element
+        const password = document.querySelectorAll(".hidden-password")[index];
+        password.textContent = newPassword;
+
+        // Hide the editPasswordmodal with fade-out animation
+        hideModalWithFadeOut(editPasswordmodal);
+      });
   });
+});
+
+// When the cancel button is clicked, hide the editPasswordmodal with fade-out animation
+cancelButton.addEventListener("click", () => {
+  hideModalWithFadeOut(editPasswordmodal);
+});
+
+// When the user clicks anywhere outside of the editPasswordmodal, close it with fade-out animation
+window.addEventListener("click", (event) => {
+  if (event.target === editPasswordmodal) {
+    hideModalWithFadeOut(editPasswordmodal);
+  }
+});
+
+// Delete Password (also needs to delete the password from the database securely)
+const deletePasswordButtons = document.querySelectorAll(".delete-password");
+const deletePasswordModal = document.getElementById("deletePasswordModal");
+const confirmDeleteButton = deletePasswordModal.querySelector(
+  ".confirm-delete-password"
+);
+const cancelDeleteButton = deletePasswordModal.querySelector(
+  ".cancel-delete-password"
+);
+
+deletePasswordButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    // Show the deletePasswordModal with fade-in animation
+    showModal(deletePasswordModal);
+
+    // When the confirm delete button is clicked, delete the password card and hide the deletePasswordModal with fade-out animation
+    confirmDeleteButton.addEventListener("click", () => {
+      // Get the password card element and remove it with fade-out animation
+      const passwordCard = button.closest(".dashboard__passwords-card");
+      passwordCard.classList.add("fade-out");
+      setTimeout(() => {
+        passwordCard.remove();
+      }, 500);
+
+      // Hide the deletePasswordModal with fade-out animation
+      hideModalWithFadeOut(deletePasswordModal);
+    });
+
+    // When the cancel button is clicked, hide the deletePasswordModal with fade-out animation
+    cancelDeleteButton.addEventListener("click", () => {
+      hideModalWithFadeOut(deletePasswordModal);
+    });
+  });
+});
+
+// When the user clicks anywhere outside of the deletePasswordModal, close it with fade-out animation
+window.addEventListener("click", (event) => {
+  if (event.target === deletePasswordModal) {
+    hideModalWithFadeOut(deletePasswordModal);
+  }
 });
