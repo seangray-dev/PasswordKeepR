f379dc269947f974f4a810af38b0847ece5970a1
Sean Gray <seangray.dev@gmail.com>
2023-05-02 08:17:36 -0400

    implement frontend code for managing org passwords and users

--- db/seedsJS/05_org_passwords.js
+++ db/seedsJS/05_org_passwords.js
@@ -1,10 +1,10 @@
 const generateSql = (encrypt) => {
   return `
 INSERT INTO org_passwords (organization_id, website_id, username, password)
-VALUES (1, 1, 'compass', '${encrypt("org1compasspass")}'),
-       (1, 2, 'facebook','${encrypt("org1fbpass")}'),
-       (1, 3, 'slack', '${encrypt("org1slackpass")}'),
-       (1, 4, 'netflix', '${encrypt("org1netflixpass")}');
+VALUES (1, 1, 'LHL-admin', '${encrypt("org1compasspass")}'),
+       (1, 2, 'LHL-admin','${encrypt("org1fbpass")}'),
+       (1, 3, 'LHL-admin', '${encrypt("org1slackpass")}'),
+       (1, 4, 'LHL-admin', '${encrypt("org1netflixpass")}');
 `;
 };
 
